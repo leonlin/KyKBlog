ce0ed9fbc7edcfeb61b41c056cf673956d154e3f
kyk <linjiansuper@gmail.com>
2012-04-11 08:53:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KykBlog::Application.config.secret_token = 'e3306057a1a627be034b8e42c6f607276f4dbfdb21668bb7b91ffdcbb39b88891aee077318a895a541c111c4e4954ace09dfb18f12b15c2f83e7c36e7038ac67'
